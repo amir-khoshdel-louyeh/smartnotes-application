4ca87d3749cd84ba8b02366932926b09a0bf312e
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-05-18 16:59:25 +0200

    fix: retire app.exec_() dinosaur, embrace exec()

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.showMaximized()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
